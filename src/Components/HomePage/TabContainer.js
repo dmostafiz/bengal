d290d17599988f2bb55579db65c22bc3283c7da3
dmostafiz <dev.mostafiz@gmail.com>
2022-10-05 20:14:00 +0600

    hmm sd

--- src/Components/HomePage/TabContainer.js
+++ src/Components/HomePage/TabContainer.js
@@ -5,16 +5,19 @@ import React from 'react'
 export default function TabContainer({ childrens }) {
     return (
         <Tabs variant='enclosed' rounded={{base: 'sm', md:'sm'}} overflow='hidden' shadow='sm'>
+          
             <TabList border={'none'}>
                 {childrens.map((item, index) => <Tab _selected={{ bg: 'gray.100', rounded: 'none' }} p={{base: 2, md: 3}} key={index}>
                    <Text as={'h6'} fontSize={{base:'12px', md: '17px'}} fontWeight='semibold'>{item.title}</Text>  
                 </Tab>)}
-            </TabList>
+            </TabList> 
+
             <TabPanels bg='gray.100'>
                 {childrens.map((item, index) => <TabPanel key={index}>
                     {item.component}
-                </TabPanel>)}
+                </TabPanel>)} 
             </TabPanels>
+
         </Tabs>
     )
 }
