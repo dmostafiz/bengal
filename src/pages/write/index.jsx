3eb8992763b6be2218fa2f193dc8b469a5d53947
dmostafiz <dev.mostafiz@gmail.com>
2022-10-12 01:25:47 +0600

    ok done

--- src/pages/write/index.jsx
+++ src/pages/write/index.jsx
@@ -1,23 +1,24 @@
-import { Box, Divider, Input, Text } from '@chakra-ui/react'
-import { Title } from '@mantine/core'
-import React, { useContext, useRef } from 'react'
+import { Box, Button, Center, Divider, Flex, Input, Text } from '@chakra-ui/react'
+import { MultiSelect, SegmentedControl, Title } from '@mantine/core'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import BloggerRightSidebar from '../../Components/blogger/BloggerRightSidebar'
 import HomeLayout from '../../Layouts/HomeLayout'
 import LayoutColumn from '../../Layouts/HomeLayout/LayoutColumn'
 import { Editor } from '@tinymce/tinymce-react';
 import { AuthModalContext } from '../../Contexts/AuthModalContext'
+import { FileButton } from '@mantine/core';
 
 
 export default function write() {
 
-  const {onOpen} = useContext(AuthModalContext)
+  const { onOpen } = useContext(AuthModalContext)
 
   const editorRef = useRef(null)
 
   const tinnyImagePickerCallback = (cb, value, meta) => {
-    return onOpen()
+    // return onOpen()
 
-    
+
     const input = document.createElement('input');
     input.setAttribute('type', 'file');
     input.setAttribute('accept', 'image/*');
@@ -52,7 +53,7 @@ export default function write() {
 
   const image_upload_handler = (blobInfo, progress) => new Promise((resolve, reject) => {
 
-    return onOpen()
+    // return onOpen()
 
     const xhr = new XMLHttpRequest();
     xhr.withCredentials = false;
@@ -94,6 +95,88 @@ export default function write() {
   })
 
 
+  const [file, setFile] = useState(null)
+
+  const [fileDataURL, setFileDataURL] = useState(null);
+
+  useEffect(() => {
+
+    console.log('UploadedFile ', file)
+
+    let fileReader, isCancel = false;
+
+    if (file) {
+      fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        const { result } = e.target;
+        if (result && !isCancel) {
+          setFileDataURL(result)
+        }
+      }
+      fileReader.readAsDataURL(file);
+    }
+    return () => {
+      isCancel = true;
+      if (fileReader && fileReader.readyState === 1) {
+        fileReader.abort();
+      }
+    }
+
+  }, [file]);
+
+
+  const data = [
+    { value: 'গল্প', label: 'গল্প' },
+    { value: 'উপন্যাস', label: 'উপন্যাস' },
+    { value: 'সমসাময়িক', label: 'সমসাময়িক' },
+    { value: 'কবিতা', label: 'কবিতা' },
+    { value: 'সাহিত্য', label: 'সাহিত্য' },
+    { value: 'পড়ালেখা', label: 'পড়ালেখা' },
+    { value: 'দেশ-বিদেশ', label: 'দেশ-বিদেশ' },
+    { value: 'ভ্রমণ', label: 'ভ্রমণ' },
+    { value: 'মুক্তিযুদ্ধ', label: 'মুক্তিযুদ্ধ' },
+    { value: 'ছবি ব্লগ', label: 'ছবি ব্লগ' },
+    { value: 'বিজ্ঞান-প্রযুক্তি', label: 'বিজ্ঞান-প্রযুক্তি' },
+  ];
+
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const [mainCategory, setMainCategory] = useState(selectedCategories[0]?.value);
+
+  const [blogType, setBlogTyple] = useState('normal')
+
+  const [stepStatus, setStepStatus] = useState('new')
+
+
+  const stepPosts = [
+    {
+      image: 'https://img.icons8.com/clouds/256/000000/futurama-bender.png',
+      label: 'Bender Bending Rodríguez',
+      value: 'Bender Bending Rodríguez',
+      description: 'Fascinated with cooking',
+    },
+  
+    {
+      image: 'https://img.icons8.com/clouds/256/000000/futurama-mom.png',
+      label: 'Carol Miller',
+      value: 'Carol Miller',
+      description: 'One of the richest people on Earth',
+    },
+    {
+      image: 'https://img.icons8.com/clouds/256/000000/homer-simpson.png',
+      label: 'Homer Simpson',
+      value: 'Homer Simpson',
+      description: 'Overweight, lazy, and often ignorant',
+    },
+    {
+      image: 'https://img.icons8.com/clouds/256/000000/spongebob-squarepants.png',
+      label: 'Spongebob Squarepants',
+      value: 'Spongebob Squarepants',
+      description: 'Not just a sponge',
+    },
+  ];
+  
+
   return (
     <HomeLayout>
 
@@ -108,14 +191,14 @@ export default function write() {
         <Box mb={8}>
 
           <Box py={2}>
-            <Title order={4}>ব্লগ লিখুন</Title>
+            <Title order={3}>ব্লগ লিখুন</Title>
           </Box>
 
           <Divider mb={4} />
 
           <Box mb={10}>
-            <Box mb={1} px={1}>
-              <Title order={6}>শিরোনাম</Title>
+            <Box mb={1} px={0}>
+              <Title order={5}>শিরোনাম</Title>
             </Box>
             <Input
               border={'1px'}
@@ -141,7 +224,7 @@ export default function write() {
               initialValue=""
 
               init={{
-                placeholder: 'এখানে বিস্তারিত পোস্ট লিখুন',
+                placeholder: 'এখানে বিস্তারিত পোস্ট লিখুন...',
                 height: 400,
                 menubar: false,
                 language: 'bn_BD',
@@ -156,7 +239,7 @@ export default function write() {
                 // toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
                 // content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
                 // a11y_advanced_options: true
-       
+
                 file_picker_callback: tinnyImagePickerCallback,
                 images_upload_handler: image_upload_handler,
 
@@ -166,13 +249,114 @@ export default function write() {
 
                 setup: (editor) => {
                   editor.on('click', () => {
-                     onOpen()
+                    //  onOpen()
                   });
                 }
               }}
             />
 
           </Box>
+
+
+          <Box mb={10}>
+            <Flex>
+              <Box flex={1}>
+                <Box mb={1} px={0}>
+                  <Title order={5}>প্রচ্ছদ ছবি</Title>
+                </Box>
+
+                <FileButton onChange={setFile} accept="image/png,image/jpeg">
+                  {(props) => <Center bg={fileDataURL ? 'gray.800' : 'gray.50'} bgImage={fileDataURL} bgSize='contain' bgPosition={'center'} bgRepeat='no-repeat' border={'2px'} borderColor='blackAlpha.100' cursor={'pointer'} {...props} w='full' h={fileDataURL ? { base: 200, md: 500 } : 150}>
+                    <Button bg={'whiteAlpha.600'} variant='outline'>{fileDataURL ? 'প্রচ্ছদ ছবি পরিবর্তন করুন' : 'প্রচ্ছদ ছবি আপলোড করুন'}</Button>
+                  </Center>}
+                </FileButton>
+                {/* <Button {...props}>Upload image</Button> */}
+              </Box>
+
+              {/* <Box flex={1}>
+
+              </Box> */}
+            </Flex>
+          </Box>
+
+
+          <Box mb={10}>
+            <Box mb={1} px={0}>
+              <Title order={5}>লেখার ক্যাটাগরি</Title>
+              <Text fontSize={'13px'} color='blackAlpha.700'>সর্বনিম্ন ১টি এবং সর্বোচ্চ ৩ টি ক্যাটাগরি নির্বাচন করতে পারবেন</Text>
+            </Box>
+
+
+            <MultiSelect
+              data={data}
+              searchable
+              nothingFound="কোনকিছু পাওয়া যায়নি"
+              maxSelectedValues={3}
+              // label={}
+              placeholder="৩ টি ক্যাটাগরি নির্বাচন করুন"
+              value={selectedCategories}
+              onChange={setSelectedCategories}
+
+              sx={{
+                '&:focus': {
+                  border: '0px'
+                }
+              }}
+            />
+
+          </Box>
+
+          <Box mb={10}>
+            <Box mb={1} px={0}>
+              <Title order={5}>লেখার ধরন</Title>
+              {/* <Text fontSize={'13px'} color='blackAlpha.700'>সর্বনিম্ন ১টি এবং সর্বোচ্চ ৩ টি ক্যাটাগরি নির্বাচন করতে পারবেন</Text> */}
+            </Box>
+
+
+            <SegmentedControl
+              value={blogType}
+              onChange={setBlogTyple}
+              data={[
+                { label: 'সাধারণ', value: 'normal' },
+                { label: 'ধারাবাহিক', value: 'multiStep' }
+              ]}
+            />
+
+          </Box>
+
+          {blogType == 'multiStep' && <Box mb={10}>
+
+            <Box mb={1} px={0}>
+              <Title order={5}>ধারাবাহিক লেখার স্ট্যাটাস</Title>
+              {/* <Text fontSize={'13px'} color='blackAlpha.700'>সর্বনিম্ন ১টি এবং সর্বোচ্চ ৩ টি ক্যাটাগরি নির্বাচন করতে পারবেন</Text> */}
+            </Box>
+
+
+            <SegmentedControl
+              value={stepStatus}
+              onChange={setStepStatus}
+              data={[
+                { label: 'নতুন শুরু করছি', value: 'new' },
+                { label: 'পূর্বের লেখা থেকে চলমান', value: 'old' }
+              ]}
+            />
+
+
+          </Box>}
+
+
+
+          {blogType == 'multiStep' && stepStatus == 'old' && <Box mb={10}>
+
+            <Box mb={1} px={0}>
+              <Title order={5}>পূর্বের ধারাবাহিক লেখা নির্বাচন করুন</Title>
+              {/* <Text fontSize={'13px'} color='blackAlpha.700'>সর্বনিম্ন ১টি এবং সর্বোচ্চ ৩ টি ক্যাটাগরি নির্বাচন করতে পারবেন</Text> */}
+            </Box>
+
+
+
+          </Box>}
+
 
         </Box>
 
