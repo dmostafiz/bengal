8ba7de03531e8d3e738b7ba93658ec7d5bb81f75
dmostafiz <dev.mostafiz@gmail.com>
2022-10-21 01:39:29 +0600

    df

--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -10,19 +10,23 @@ export const AuthContext = createContext()
 const AuthContextProvider = ({ children }) => {
 
 
+    const [hasUser, setHasUser] = useState(false)
+
     const { data, isLoading, isError, error } = useQuery(['authUser'], async () => {
 
         const response = await Axios.get('/auth/get_authorised_user')
 
         // console.log('Authorized User: ', response?.data?.user)
-
+        setHasUser(true)
         return response?.data?.user
+    }, {
+        enabled: !hasUser ? true : false,
     })
 
     const logoutUser = async () => {
         // alert('logout')
         const response = await Axios.post('/auth/logout')
-        if(response?.data?.ok){
+        if (response?.data?.ok) {
             removeAccessToken()
 
             window.location.href = '/'
