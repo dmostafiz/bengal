75c5299a26c44bd1b573e595365a6de25d8173fb
dmostafiz <dev.mostafiz@gmail.com>
2022-10-22 12:25:12 +0600

    df

--- src/Components/Common/FullscreenLoader.jsx
+++ src/Components/Common/FullscreenLoader.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 export default function FullscreenLoader({spinnerSize='xl', text='অপেক্ষা করুন...', showText=true}) {
     return (
-        <Center bg='blackAlpha.900' w='100vw' h='100vh' right={0} top={0} zIndex={999999999999} pos={'fixed'} >
-            <Box bg={'green.40'} px={3} py={2} rounded='full' shadow='md'>
+        <Center bg='blackAlpha.600' w='100vw' h='100vh' right={0} top={0} zIndex={999999999999} pos={'fixed'} >
+            <Box px={3} py={2} rounded='full'>
                 <Flex alignItems={'center'} gap={3}>
                     <Spinner size={spinnerSize} color='white' />
-                    {(showText && text) && <Text color='white'>{text}</Text>}
+                    {(showText && text) && <Text color='white' fontWeight={'bold'}>{text}</Text>}
                 </Flex>
             </Box>
         </Center>
--- src/Components/Common/SiteLogoDesktop.jsx
+++ src/Components/Common/SiteLogoDesktop.jsx
@@ -1,10 +1,13 @@
 import { Box, Center, Image } from '@chakra-ui/react'
+import Link from 'next/link'
 import React from 'react'
 
 export default function SiteLogoDesktop() {
     return (
-        <Box my={3} h={{base: '55px', md: '60px', lg:'70px'}} w={{base: '110px', md: '120px', lg: '200px'}}>
-          <Image h='full' w='full' src='logo.png' />
-        </Box>
+        <Link href={'/'}>
+            <Box cursor={'pointer'} my={3} h={{ base: '55px', md: '60px', lg: '70px' }} w={{ base: '110px', md: '120px', lg: '200px' }}>
+                <Image h='full' w='full' src='/logo.png' />
+            </Box>
+        </Link>
     )
 }
