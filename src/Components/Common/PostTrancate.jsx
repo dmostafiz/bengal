0178520140f4d8100babee84668557e78265bef1
dmostafiz <dev.mostafiz@gmail.com>
2022-11-03 19:26:44 +0600

    hr

--- src/Components/Common/PostTrancate.jsx
+++ src/Components/Common/PostTrancate.jsx
@@ -7,17 +7,18 @@ import truncate from 'truncate-html';
 
 export default function PostTrancate({ content, slug, lines = 3 }) {
     return (
-
-        <Box as='div' w='full' fontSize={'17px'} dangerouslySetInnerHTML={{
-            __html: truncate(content, 270, {
-                ellipsis: `...`,
-                // byWords: true,
-                keepWhitespaces: true,
-                stripTags: false,
-                excludes: ['img', 'video'],
-                decodeEntities: true,
-                reserveLastWord: true
-            })
-        }} />
+       <Box w='full'>
+           <Text as='div' w='full' fontSize={'17px'} dangerouslySetInnerHTML={{
+               __html: truncate(content, 270, {
+                   ellipsis: `...`,
+                   // byWords: true,
+                   keepWhitespaces: true,
+                   stripTags: false,
+                   excludes: ['img', 'video'],
+                   decodeEntities: true,
+                   reserveLastWord: true
+               })
+           }} />
+       </Box>
     )
 }
