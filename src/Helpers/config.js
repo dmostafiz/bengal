b0726a62f7ab7c870ffd77f17ba4d05bf676d918
dmostafiz <dev.mostafiz@gmail.com>
2022-11-11 21:57:30 +0600

    done

--- src/Helpers/config.js
+++ src/Helpers/config.js
@@ -1,4 +1,4 @@
 export const siteName = 'সামান্তরিক ব্লগ'
 export const siteSlogun = 'বাংলা ভাষায় হৃদয়ের স্পন্দন'
-export const imageUrl = 'http://shamantorik.vercel.app/logo.png'
+export const imageUrl = 'https://www.shamantorik.com/main_logo.jpg'
 export const siteUrl = 'http://shamantorik.vercel.app'
