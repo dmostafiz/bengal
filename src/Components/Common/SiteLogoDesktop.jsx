ddf57284d112e1a4561272ad800ca82641ae7e47
dmostafiz <dev.mostafiz@gmail.com>
2022-11-14 19:18:47 +0600

    gfg

--- src/Components/Common/SiteLogoDesktop.jsx
+++ src/Components/Common/SiteLogoDesktop.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function SiteLogoDesktop() {
     return (
         <Link href={'/'}>
-            <Box cursor={'pointer'} my={3} h={{ base: '44px', md: '60px', lg: '70px' }} w={{ base: '110px', md: '180px', lg: '200px' }}>
+            <Box cursor={'pointer'} my={3} h={{ base: '48px', md: '60px', lg: '70px' }} w={{ base: '120px', md: '180px', lg: '200px' }}>
                 <Image h='full' w='full' src='/logo.png' />
             </Box>
         </Link>
