6aae5cb8248a06e685f403bd8626856d8cd93924
dmostafiz <dev.mostafiz@gmail.com>
2022-11-15 16:04:55 +0600

    dfdf

--- src/Layouts/Common/MainLeftSidebar.jsx
+++ src/Layouts/Common/MainLeftSidebar.jsx
@@ -42,7 +42,7 @@ export default function MainLeftSidebar({ authpanel = true }) {
                 </Box>
             </Box> */}
 
-            <Box w='full' bg='' mb={5}>
+           {onlineUsers.length > 0 && <Box w='full' bg='' mb={5}>
 
                 <Box bg='blackAlpha.50' p={2}>
                     <Title order={6}>{onlineUsers.length
@@ -90,7 +90,7 @@ export default function MainLeftSidebar({ authpanel = true }) {
 
                 </Box>
 
-            </Box>
+            </Box>}
 
 
 
