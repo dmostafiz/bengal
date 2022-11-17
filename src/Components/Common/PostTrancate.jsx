90a5014bcf14f69a621aa706188309ddc0698968
dmostafiz <dev.mostafiz@gmail.com>
2022-11-17 22:48:20 +0600

    FGFG

--- src/Components/Common/PostTrancate.jsx
+++ src/Components/Common/PostTrancate.jsx
@@ -8,7 +8,7 @@ import truncate from 'truncate-html';
 export default function PostTrancate({image='', content, slug, char = 270 }) {
     return (
         <Box w='full'>
-            <Text as='div' w='full' align={'justify'} noOfLines={{base:5, xl: image ? 6 : 5 }} dangerouslySetInnerHTML={{
+            <Text as='div' w='full' align={'justify'} noOfLines={{base:5, xl: image ? 7 : 5 }} dangerouslySetInnerHTML={{
                 __html: truncate(content, char, {
                     ellipsis: `...`,
                     byWords: true,
